9cd1d7b06f666e6594c7d591adaabae0c7144b3e
Koopa <koopapapa@gmail.com>
2023-12-01 10:32:25 +0800

    Upgrade Go version and refactor random string method
    The Go version has been upgraded from 1.19 to 1.21 across all modules to leverage improvements in the latest version. Additionally, the method for generating a random string in the toolkit has been refactored for enhanced readability and potentially better performance.

--- tools.go
+++ tools.go
@@ -20,11 +20,15 @@ type Tools struct {
 
 func (t *Tools) RandomString(n int) string {
 
-	s, r := make([]rune, n), []rune(randomStringSource)
-	for i := range s {
-		p, _ := rand.Prime(rand.Reader, len(r))
-		x, y := p.Uint64(), uint64(len(r))
-		s[i] = r[x%y]
+	b := make([]byte, n)
+
+	if _, err := rand.Read(b); err != nil {
+		return ""
+	}
+
+	s := make([]rune, n)
+	for i, b := range b {
+		s[i] = rune(randomStringSource[int(b)%len(randomStringSource)])
 	}
 
 	return string(s)
